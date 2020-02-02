180dab14018487350fd4f508a8254a22afa9de74
Jean-Jacques Bernard <jjb@cantab.net>
2020-02-02 11:58:22 +0100

    Broken curses code. Need to update it.

--- Days 01-03/03.py
+++ Days 01-03/03.py
@@ -2,30 +2,55 @@
 from datetime import datetime, timedelta
 from time import sleep
 import os
+import curses
+from curses import wrapper
 
 
-def get_durations():
+def main(stdscr):
+    stdscr.clear()
+
+    run_pomodoro(stdscr)
+
+    stdscr.refresh()
+    stdscr.getkey()
+
+
+def curses_input(stdscr, x, y, string_input, size):
+    curses.echo()
+    stdscr.addstr(x, y, string_input)
+    stdscr.refresh()
+    input = stdscr.getstr(x + 1, y, size)
+    return input
+
+
+def get_durations(stdscr):
     """
         Get the duration for both the quiet time to focus and the pause between those
         quiet times
-    :return: a list with 2 elements: quiet time and pause time durations
+    :return: a list with 2 elements:
+            - quiet_time
+            - pause_time
     """
 
     while True:
-        quiet_time = input("How long would you like your focus time to last (in min)? ")
+        stdscr.clear()
+        quiet_time = curses_input(stdscr, 0, 0, "How long would you like your focus time to last (in min)? ", 2)
 
         try:
             quiet_time = int(quiet_time)
         except ValueError:
-            print("You should enter a number of minutes.")
+            stdscr.addstr(1, 0, "You should enter a number of minutes.")
+            stdscr.refresh()
             continue
 
-        pause_time = input("How long would you like your pause time to last (in min)? ")
+        stdscr.clear()
+        pause_time = curses_input(stdscr, 0, 0, "How long would you like your pause time to last (in min)? ", 1)
 
         try:
             pause_time = int(pause_time)
         except ValueError:
-            print("You should enter a number of minutes.")
+            stdscr.addstr(1, 0, "You should enter a number of minutes.")
+            stdscr.refresh()
             continue
 
         break
@@ -33,18 +58,19 @@ def get_durations():
     return [quiet_time, pause_time]
 
 
-def run_pomodoro():
+def run_pomodoro(stdscr):
     while True:
-        answer = input("Would you like to specify Pomodoro times? [Y/N] ")
+        stdscr.clear()
+        answer = curses_input(stdscr, 0, 0, "Would you like to specify Pomodoro times? [Y/N] ", 1)
 
         if answer not in ['Y', 'y', 'N', 'n']:
-            print("Answer must be Y (for Yes) or N (for No).")
+            stdscr.addstr(1, 0, "Answer must be Y (for Yes) or N (for No).")
             continue
         else:
             break
 
     if answer in ['Y', 'y']:
-        q, p = get_durations()
+        q, p = get_durations(stdscr)
     else:
         q, p = 25, 5
 
@@ -52,30 +78,30 @@ def run_pomodoro():
     qtime = timedelta(minutes=q)
     ptime = timedelta(minutes=p)
 
-    os.system("CLEAR")
+    # os.system("CLEAR")
     while True:
-        print("Focus time starting!\n")
-        sleep(10)
+        stdscr.clear()
+        stdscr.addstr(0,0, "Focus time!")
         while datetime.today() < starttime + qtime:
             sleep(1)
             remain = starttime + qtime - datetime.today()
             minutes = remain.seconds // 60
             seconds = remain.seconds % 60
-            os.system("CLEAR")
-            print("Focus time. Time left --> {}:{}".format(str(minutes), str(seconds)))
+            # os.system("CLEAR")
+            stdscr.addstr(1, 0, "Focus time. Time left --> {}:{}".format(str(minutes), str(seconds)))
 
-        print("\nEnd of your quiet time. You can take a break\n")
-        sleep(10)
+        stdscr.addstr(2, 0, "End of your quiet time. You can take a break")
         while datetime.today() < starttime + qtime + ptime:
             sleep(1)
             remain = starttime + qtime + ptime - datetime.today()
             minutes = remain.seconds // 60
             seconds = remain.seconds % 60
-            os.system("CLEAR")
-            print("Time left for break --> {}:{}".format(str(minutes), str(seconds)), end="\r", flush=False)
+            # os.system("CLEAR")
+            stdscr.addstr(3, 0, "Time left for break --> {}:{}".format(str(minutes), str(seconds)))
 
         starttime = datetime.today()
 
 
 if __name__ == "__main__":
-    run_pomodoro()
+    stdscr = curses.initscr()
+    wrapper(main(stdscr))
