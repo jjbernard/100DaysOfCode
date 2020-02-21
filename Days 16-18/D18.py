3cb13556ba29d94ff92750265482c186c9ea5b14
Jean-Jacques Bernard <jjb@cantab.net>
2020-02-21 22:20:03 +0100

    Day 18

---
+++ Days 16-18/D18.py
@@ -0,0 +1,51 @@
+NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
+         'julian sequeira', 'sandra bullock', 'keanu reeves',
+         'julbob pybites', 'bob belderbos', 'julian sequeira',
+         'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
+
+bites = {6: 'PyBites Die Hard',
+         7: 'Parsing dates from logs',
+         9: 'Palindromes',
+         10: 'Practice exceptions',
+         11: 'Enrich a class with dunder methods',
+         12: 'Write a user validation function',
+         13: 'Convert dict in namedtuple/json',
+         14: 'Generate a table of n sequences',
+         15: 'Enumerate 2 sequences',
+         16: 'Special PyBites date generator',
+         17: 'Form teams from a group of friends',
+         18: 'Find the most common word',
+         19: 'Write a simple property',
+         20: 'Write a context manager',
+         21: 'Query a nested data structure'}
+exclude_bites = {6, 10, 16, 18, 21}
+
+
+def filter_bites(bites=bites, bites_done=exclude_bites):
+    """return the bites dict with the exclude_bites filtered out"""
+    return {k: v for k, v in bites.items() if k not in exclude_bites}
+
+
+def dedup_and_title_case_names(names):
+    """Should return a list of title cased names,
+       each name appears only once"""
+    return [name.title() for name in set(names)]
+
+
+def sort_by_surname_desc(names):
+    """Returns names list sorted desc by surname"""
+    names = dedup_and_title_case_names(names)
+    names.sort(key=lambda x: x.split()[1], reverse=True)
+    return names
+
+
+def shortest_first_name(names):
+    """Returns the shortest first name (str).
+       You can assume there is only one shortest name.
+    """
+    names = dedup_and_title_case_names(names)
+    return min([name.split()[0] for name in names], key=len)
+
+
+if __name__ == '__main__':
+    print(shortest_first_name(NAMES))
