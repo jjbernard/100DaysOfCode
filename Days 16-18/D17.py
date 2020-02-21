d3376766532ae940d05deee4c2eddb8816131818
Jean-Jacques Bernard <jjb@cantab.net>
2020-02-21 21:06:27 +0100

    Using itertools and create the name_list into the gen_pairs() function

--- Days 16-18/D17.py
+++ Days 16-18/D17.py
@@ -1,4 +1,5 @@
 import random
+import itertools
 
 NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
          'julian sequeira', 'sandra bullock', 'keanu reeves',
@@ -10,13 +11,13 @@ name_list = [name.title() for name in NAMES]
 name_reversed = [" ".join(list(reversed(name.split()))) for name in name_list]
 
 
-def gen_pair(nl):
-    for i in range(50):
-        pairs = random.choices(nl, k=2)
+def gen_pair():
+    name_list = [name.title() for name in NAMES]
+    while True:
+        pairs = random.choices(name_list, k=2)
         yield f'{pairs[0].split()[0]} teams up with {pairs[1].split()[0]}'
 
 
 if __name__ == '__main__':
-    pairs = gen_pair(name_list)
-    for _ in range(10):
-        print(next(pairs))
+    pairs = gen_pair()
+    print(list(itertools.islice(pairs, 10)))
