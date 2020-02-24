5e1339079c25a5f34e74019fd8b81673b8bddc9d
Jean-Jacques Bernard <jjb@cantab.net>
2020-02-24 20:14:59 +0100

    Differentiate between colors

--- Days 19-21/D20.py
+++ Days 19-21/D20.py
@@ -3,12 +3,25 @@ import sys
 import time
 import random
 
+
+def traffic_light(light_rotation):
+    for color in light_rotation:
+        if color == 'red':
+            sys.stdout.write('\r' + 'Red')
+            sys.stdout.flush()
+            time.sleep(random.randint(2, 4))
+        elif color == 'amber':
+            sys.stdout.write('\r' + 'Amber')
+            sys.stdout.flush()
+            time.sleep(1)
+        else:
+            sys.stdout.write('\r' + 'Green')
+            sys.stdout.flush()
+            time.sleep(random.randint(3, 6))
+
+
 if __name__ == '__main__':
     traffic_colors = 'green amber red'.split()
+    light_rotation = cycle(traffic_colors)
 
-    traffic_light = cycle(traffic_colors)
-
-    while True:
-        sys.stdout.write('\r' + next(traffic_light))
-        sys.stdout.flush()
-        time.sleep(random.randint(1, 4))
+    traffic_light(light_rotation)
