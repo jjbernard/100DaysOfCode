7415dc5dd6f3ffb19301c144f8637eebe519c242
Jean-Jacques Bernard <jjb@cantab.net>
2020-03-22 12:06:13 +0100

    Start simple CSV analysis of 538 dataset

---
+++ Days 37-39/D39.py
@@ -0,0 +1,8 @@
+"""
+In this file, we will analyse data coming from the FiveThirtyEight github
+repo located at https://github.com/fivethirtyeight.
+More specifically, we will look at the "congress-age" dataset which records
+all US congress members from Jan 1947 to Feb 2014.
+The readme from 538 is here:
+https://github.com/fivethirtyeight/data/tree/master/congress-age
+"""
